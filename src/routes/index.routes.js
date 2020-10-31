70a823011afc17dbfcef4d93b6041e82d0f97b59
dayism <31575151+dayism@users.noreply.github.com>
2020-10-30 21:45:15 -0500

    5

---
+++ src/routes/index.routes.js
@@ -0,0 +1,6 @@
+const {Router} = require("express");
+const router=  Router();
+
+const {renderIndex,renderAbout}= require('../controllers/index.controllers');
+router.get('/', renderIndex) ;
+module.exports = router;
