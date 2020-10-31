d5270596fbb24b52373d2f1aacaf6a9f76ad9b46
dayism <31575151+dayism@users.noreply.github.com>
2020-10-31 12:14:34 -0500

    cambio 6

--- src/server.js
+++ src/server.js
@@ -25,7 +25,7 @@ app.use( methodOverride('method'));
 // routes
 
 app.use(require('./routes/paginasWeb.routes'));
-//app.use(require('./routes/index.routes'));
+app.use(require('./routes/index.routes'));
 
 
 module.exports = app;
